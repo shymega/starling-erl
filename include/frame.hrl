124d66583187a8e05849ec56f1e7edc0b992ca73
Dom Rodriguez <shymega@shymega.org.uk>
2020-08-12 19:39:49 +0100

    [refactor](include): Remove old include header
    This was originally for Stomperl, I think I copied it to this Erlang codebase for a test.
    I can't remember now, but hey.

--- include/frame.hrl
+++
@@ -1,8 +0,0 @@
--record(frame,
-        {cmd :: binary(),
-         headers :: map(),
-         content_type :: binary(),
-         body :: binary()}).
-
--define(DEFAULT_STOMP_VERSION, 1.2).
--define(KV_RE,"^([a-zA-Z0-9\-_]*):(.*)$").
